56760e01680e3aebb57aaf4fcbc6d35f489ed33d
Okan Uregen <okan.uregen@isik.edu.tr>
2021-04-07 16:20:11 +0300

    Add car java part done (not db)

--- app/src/main/java/com/example/b2crentacar/AddCarActivity.java
+++ app/src/main/java/com/example/b2crentacar/AddCarActivity.java
@@ -7,8 +7,14 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -16,14 +22,31 @@ public class AddCarActivity extends AppCompatActivity {
     private Uri filePath;
     private final int PICK_IMAGE_REQUEST = 71;
     private ImageView carImage;
+    private Button addCar;
+    private EditText plateNumber,brand,model,price,year;
+    private String PlateNumber,Brand,Model,Price,Year;
+    private RadioGroup radioGroupType, radioGroupGear, radioGroupFuelType;
+    private RadioButton RadioButtonType, RadioButtonGear, RadioButtonFuelType;
+    private String RadioTypeText, RadioGearText, RadioFuelTypeText;
+    private Button addCarBtn;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_car);
-
         carImage=findViewById(R.id.carImage);
+        plateNumber = findViewById(R.id.txtPlateNo);
+        brand = findViewById(R.id.txtBrand);
+        model = findViewById(R.id.txtModel);
+        price = findViewById(R.id.txtPrice);
+        year = findViewById(R.id.txtYear);
+        radioGroupType = findViewById(R.id.radioGroup3);
+        radioGroupGear = findViewById(R.id.radioGroup);
+        radioGroupFuelType = findViewById(R.id.radioGroup5);
+        addCarBtn = (Button) findViewById(R.id.btnAddCar);
+
 
 
         carImage.setOnClickListener(new View.OnClickListener() {
@@ -33,7 +56,61 @@ public class AddCarActivity extends AppCompatActivity {
             }
         });
 
+        addCarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlateNumber = plateNumber.getText().toString();
+                Brand = brand.getText().toString();
+                Model = model.getText().toString();
+                Price = price.getText().toString();
+                Year = year.getText().toString();
 
+                int selectedIdType = radioGroupType.getCheckedRadioButtonId();
+                RadioButtonType = findViewById(selectedIdType);
+                RadioTypeText = RadioButtonType.getText().toString();
+
+                int selectedIdGear = radioGroupGear.getCheckedRadioButtonId();
+                RadioButtonGear = findViewById(selectedIdGear);
+                RadioGearText = RadioButtonGear.getText().toString();
+
+                int selectedIdFuelType = radioGroupFuelType.getCheckedRadioButtonId();
+                RadioButtonFuelType = findViewById(selectedIdFuelType);
+                RadioFuelTypeText = RadioButtonFuelType.getText().toString();
+
+                if(checkInput()){
+                    //do the db stuffs
+
+                }
+
+            }
+        });
+
+    }
+
+    private boolean checkInput(){
+        if(!TextUtils.isEmpty(PlateNumber)){
+            if(!TextUtils.isEmpty(Brand)){
+                if(!TextUtils.isEmpty(Model)){
+                    if(!TextUtils.isEmpty(Price)){
+                        if(!TextUtils.isEmpty(Year)){
+                            return true;
+                        }else{
+                            year.setError("Please Enter Year");
+                        }
+                    }else{
+                        price.setError("Please Enter Price");
+                    }
+                }else{
+                    model.setError("Please Enter Model");
+                }
+            }else{
+                brand.setError("Please Enter Brand");
+            }
+        }else{
+            plateNumber.setError("Please Enter Plate Number");
+        }
+
+        return false;
 
     }
 
